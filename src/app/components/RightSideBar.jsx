e00633d27204325c05cace1c4bc73ab6f6a2854f
Prateek Bachwani <bachwani.prateek@gmail.com>
2025-09-22 00:08:03 +0530

    right panel changes

--- src/app/components/RightSideBar.jsx
+++ src/app/components/RightSideBar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const RightSideBar = ({ onGenerate, isGenerating }) => {
   //   console.log("is generating", isGenerating);
   return (
-    <aside className="w-80 bg-[#1C1F22] p-4 flex flex-col gap-6">
+    <aside className="w-80 bg-[#1C1F22] p-4 flex flex-col gap-4">
       <div>
         <button className="bg-[#333124] text-[#FFD233] w-fit px-2 py-1 rounded-sm">
           Generate
@@ -12,7 +12,7 @@ const RightSideBar = ({ onGenerate, isGenerating }) => {
         <button className="ml-4">Execute</button>
       </div>
       <div className="flex flex-col">
-        <p>PRD</p>
+        {!isGenerating && <p>PRD</p>}
         <div className="w-full flex justify-between items-center bg-[#25292D] px-2 rounded-lg mt-2">
           <input
             type="text"
@@ -21,11 +21,13 @@ const RightSideBar = ({ onGenerate, isGenerating }) => {
           />
           <span>✅</span>
         </div>
-        <span className="text-right text-[#BACCE3]">+ add more</span>
+        {!isGenerating && (
+          <span className="text-right text-[#BACCE3]">+ add more</span>
+        )}
       </div>
 
       <div className="flex flex-col">
-        <p>Figma File</p>
+        {!isGenerating && <p>Figma File</p>}
         <div className="w-full flex justify-between items-center bg-[#25292D] px-2 rounded-lg mt-2">
           <input
             type="text"
@@ -35,53 +37,54 @@ const RightSideBar = ({ onGenerate, isGenerating }) => {
           <span>✅</span>
         </div>
       </div>
+      {!isGenerating && (
+        <>
+          <div className="flex flex-col">
+            <p>Branch</p>
+            <div className="w-full flex justify-between items-center bg-[#25292D] px-2 rounded-lg mt-2">
+              <input
+                type="text"
+                defaultValue={"+ Link Branch"}
+                className="py-2 w-full text-[#BACCE3]"
+              />
+              <span>✅</span>
+            </div>
+          </div>
 
-      <div className="flex flex-col">
-        <p>Branch</p>
-        <div className="w-full flex justify-between items-center bg-[#25292D] px-2 rounded-lg mt-2">
-          <input
-            type="text"
-            defaultValue={"+ Link Branch"}
-            className="py-2 w-full text-[#BACCE3]"
-          />
-          <span>✅</span>
-        </div>
-      </div>
+          <div className="flex flex-col">
+            <p>LLM Model</p>
+            <div className="w-full flex justify-between items-center bg-[#16191B] px-2 rounded-lg mt-2">
+              <input
+                type="text"
+                defaultValue={"Auto Selected"}
+                className="py-2 w-full "
+              />
 
-      <div className="flex flex-col">
-        <p>LLM Model</p>
-        <div className="w-full flex justify-between items-center bg-[#16191B] px-2 rounded-lg mt-2">
-          <input
-            type="text"
-            defaultValue={"Auto Selected"}
-            className="py-2 w-full "
-          />
+              <span className="rotate-90">{">"}</span>
+            </div>
+          </div>
 
-          <span className="rotate-90">{">"}</span>
-        </div>
-      </div>
-
-      <div className="">
-        <button
-          className="text-left text-black bg-gray-100 p-2 rounded-lg hover:bg-gray-800 duration-200 ease-out hover:text-white"
-          onClick={onGenerate}
-          disabled={isGenerating}
-        >
-          Generate
-        </button>
-      </div>
+          <div className="">
+            <button
+              className="text-left text-black bg-gray-100 p-2 rounded-lg hover:bg-gray-800 duration-200 ease-out hover:text-white"
+              onClick={onGenerate}
+              disabled={isGenerating}
+            >
+              Generate
+            </button>
+          </div>
+        </>
+      )}
       {/* Add inputs here for PRD file, branch, etc. */}
 
       {isGenerating && (
-        <div className="mt-4 p-3 bg-[#333124] rounded">
-          <div className="flex items-center space-x-2">
+        <>
+          <div className="flex justify-between items-center">
+            <span className="text-xs">Generating test cases</span>
             <div className="w-2 h-2 bg-yellow-400 rounded-full animate-pulse" />
-            <span className="text-xs text-[#FFD233]">
-              Generating test cases
-            </span>
-            <div className="w-3 h-3 border border-[#FFD233] border-t-transparent rounded-full animate-spin" />
           </div>
-        </div>
+          <div className="p-3 rounded border border-[] h-full"></div>
+        </>
       )}
     </aside>
   );
